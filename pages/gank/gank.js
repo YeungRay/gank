639d46622e96bb0581e3894dfe4e041952d06413
your_usernamerayyeung <986531811@qq.com>
2016-10-11 18:15:47 +0800

    小改动

--- pages/gank/gank.js
+++ pages/gank/gank.js
@@ -1,21 +1,39 @@
-Page({
-  data:{
+Page( {
+  data: {
     tems: [],
-    hidden: false
+    hidden: true,
+    // tab切换
+    currentTab: 0
   },
-  onLoad:function(options){
+  onLoad: function( options ) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function() {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function() {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function() {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function() {
     // 页面关闭
+  },
+  switchNav: function( e ) {
+    var current = e.target.dataset.current;
+    if( this.data.currentTab == current ) {
+      return false;
+    } else {
+      this.setData( {
+        currentTab: current
+      })
+    }
+  },
+  swiperChange: function(e) {
+    var current = e.detail.current;
+    this.setData( {
+        currentTab: current
+      })
   }
 })
